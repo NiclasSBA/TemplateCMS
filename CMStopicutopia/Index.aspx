ea17782e3ca386b9bb5204256c86d48bee9e873a
Niclas Andersen <niclas_andersen@hotmail.com>
2018-02-17 12:41:57 +0100

    Initial commit for 1st assignment
    Signed-off-by: Niclas Andersen <niclas_andersen@hotmail.com>

---
+++ CMStopicutopia/Index.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="CMStopicutopia.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Complex Controls</title>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+        <asp:Label ID="statusInfo" runat="server"></asp:Label>
+        <div>
+            <asp:Image ID="Image1" runat="server" ImageUrl="~/Images/download.jpg" />
+            
+            <asp:Button ID="ButtonChangeImage" runat="server" Text="ChangePicture" OnClick="ButtonChangeImage_Click" />
+            <asp:Label ID="LabelControls" runat="server" Text="Calendar Controls"></asp:Label>
+            <asp:Calendar ID="Calendar" runat="server" BackColor="White" BorderColor="Black" DayNameFormat="Shortest" Font-Names="Times New Roman" Font-Size="10pt" ForeColor="Black" Height="220px" NextPrevFormat="FullMonth" OnSelectionChanged="Calendar_SelectionChanged" TitleFormat="Month" Width="400px">
+                <DayHeaderStyle BackColor="#CCCCCC" Font-Bold="True" Font-Size="7pt" ForeColor="#333333" Height="10pt" />
+                <DayStyle Width="14%" />
+                <NextPrevStyle Font-Size="8pt" ForeColor="White" />
+                <OtherMonthDayStyle ForeColor="#999999" />
+                <SelectedDayStyle BackColor="#CC3333" ForeColor="White" />
+                <SelectorStyle BackColor="#CCCCCC" Font-Bold="True" Font-Names="Verdana" Font-Size="8pt" ForeColor="#333333" Width="1%" />
+                <TitleStyle BackColor="Black" Font-Bold="True" Font-Size="13pt" ForeColor="White" Height="14pt" />
+                <TodayDayStyle BackColor="#CCCC99" />
+            </asp:Calendar>
+            <asp:Label ID="LabelSelection" runat="server" Text="Selected Date"></asp:Label>
+            <asp:TextBox ID="TextBoxDate" runat="server"></asp:TextBox>
+            <asp:Label ID="LabelWeek" runat="server" Text="Week"></asp:Label>
+            <asp:TextBox ID="TextBoxDay" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:FileUpload ID="FileUpload" runat="server" />
+            <br />
+            <asp:Label ID="LabelNewFileName" runat="server" Text="New Filename"></asp:Label>
+            <asp:TextBox ID="TextBoxNewName" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="ButtonUpload" runat="server" Text="Upload to AppData" Height="39px" OnClick="ButtonUpload_Click" Width="191px" />
+        
+            <asp:Label ID="LabelName" runat="server" Text="Name" ></asp:Label>
+            <asp:TextBox ID="TextboxName" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelNormalInfo" runat="server" Text="Description"></asp:Label>
+            <asp:TextBox ID="TextboxNormalInfo" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelExtraInfo" runat="server" Text="Extra info:"></asp:Label>
+            <asp:TextBox ID="TextboxExtraInfo" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelOrigin" runat="server" Text="Origin"></asp:Label>
+            <asp:TextBox ID="TextboxOrigin" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelMainCat" runat="server" Text="Main Category"></asp:Label>
+            <asp:TextBox ID="TextboxMainCat" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelManagerChoice" runat="server" Text="Item Status"></asp:Label>
+            <asp:TextBox ID="TextboxManagerChoice" runat="server"></asp:TextBox>
+
+
+
+        </div>
+        <p>
+            <asp:TextBox ID="TextBoxInfo" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="LabelUpload" runat="server" Text=" File Upload"></asp:Label>
+            </p>
+    </form>
+</body>
+</html>
---
+++ CMStopicutopia/Index.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace CMStopicutopia
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(@"data source = .\sqlexpress; integrated security = true; database = CMS");
+        SqlDataAdapter da = null;
+        SqlCommand cmd;
+        DataSet ds = null;
+        DataTable dt = null;
+        string sqlsel = "SELECT * FROM items";
+       
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonChangeImage_Click(object sender, EventArgs e)
+        {
+            if (Image1.ImageUrl == "~/Images/download.jpg")
+            {
+                Image1.ImageUrl = "~/Images/catsuperior.jpg";
+            }
+            else if (Image1.ImageUrl == "~/Images/catsuperior.jpg")
+            {
+                Image1.ImageUrl = "~/Images/download.jpg";
+            }
+            else {
+
+                Image1.ImageUrl = "~/Images/download.jpg";
+            }
+        }
+
+
+
+        protected void Calendar_SelectionChanged(object sender, EventArgs e)
+        {
+            TextBoxDate.Text = Calendar.SelectedDate.ToShortDateString();
+            TextBoxDay.Text = Calendar.SelectedDate.DayOfWeek.ToString();
+        }
+
+        protected void ButtonUpload_Click(object sender, EventArgs e)
+        {
+            if (FileUpload.HasFile)
+            {
+                if (TextBoxNewName.Text != "")
+                {
+                   // TextBoxNewName.Text.Trim
+                        if(TextBoxNewName.Text.Substring(0,1) != " ")
+                    {
+
+                        try
+                        {
+
+                            string sqlins = "insert into Items values ('',@name, @normalInfo, @extraInfo, @origin, @main_Cat, @managers_choice_state, @image)";
+                            string filename = FileUpload.PostedFile.FileName;
+                            string path = Convert.ToString(FileUpload.PostedFile.FileName);
+                            string extension = Path.GetExtension(path);
+
+
+
+                            FileUpload.SaveAs(Server.MapPath("~/App_Data/" + TextBoxNewName.Text + extension));
+                            TextBoxNewName.Text = "";
+                            TextBoxInfo.Text = "File uploaded, brother " + FileUpload.PostedFile.FileName + FileUpload.PostedFile.ContentLength.ToString() + FileUpload.PostedFile.ContentType;
+
+                            try
+                            {
+
+                                da = new SqlDataAdapter();
+                                da.SelectCommand = new SqlCommand(sqlsel, conn);
+                                ds = new DataSet();
+                                da.Fill(ds, "MyItems");
+                                dt = ds.Tables["MyItems"];
+
+
+
+                                DataRow newrow = dt.NewRow();
+                                newrow["name"] = TextboxName.Text;
+                                newrow["normalInfo"] = TextboxNormalInfo.Text;
+                                newrow["extraInfo"] = TextboxExtraInfo.Text;
+                                newrow["origin"] = Convert.ToInt32(TextboxOrigin.Text);
+                                newrow["main_Cat"] = Convert.ToInt32(TextboxMainCat.Text);
+                                newrow["managers_choice_state"] = Convert.ToInt32(TextboxManagerChoice.Text);
+                                newrow["image"] = Convert.ToString(filename);
+                                dt.Rows.Add(newrow);
+
+
+                                cmd = new SqlCommand(sqlins, conn);
+                                cmd.Parameters.Add("@name", SqlDbType.Text, 50, "name");
+                                cmd.Parameters.Add("@normalInfo", SqlDbType.Text, 200, "normalInfo");
+                                cmd.Parameters.Add("@extraInfo", SqlDbType.Text, 200, "extraInfo");
+                                cmd.Parameters.Add("@origin", SqlDbType.Int, 4, "origin");
+                                cmd.Parameters.Add("@main_Cat", SqlDbType.Int, 4, "main_Cat");
+                                cmd.Parameters.Add("@managers_choice_state", SqlDbType.Int, 4, "managers_choice_state");
+                                cmd.Parameters.Add("@image", SqlDbType.Text, 200, "image");
+
+                                da.InsertCommand = cmd;
+                                da.Update(ds, "MyItems");
+                            }
+                            catch (Exception ex) {
+
+                                statusInfo.Text = ex.Message + ex.StackTrace;
+                            }
+
+
+
+
+                            //   GridViewShippers.DataSource = dt;
+                            //  GridViewShippers.DataBind();
+                        }
+                        catch(Exception ex) {
+                            statusInfo.Text = ex.Message + ex.StackTrace;
+                        }
+                        
+                    }
+                    else { TextBoxNewName.Text = "Intial space not allowed"; }
+
+
+                    
+                }
+                else
+                {
+                    TextBoxInfo.Text = "New filename missing, brother";
+                }
+            }
+            else {
+                TextBoxInfo.Text = "Wrong Filename, brother";
+            }
+            }
+    }
+}
